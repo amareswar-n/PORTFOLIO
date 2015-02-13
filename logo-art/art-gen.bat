235614505dde85ac074da5fb83099455434bd7d2
ＡｍＡｒ <amareswar.n@gmail.com>
2015-02-14 10:00:00 +1100

    generate logo art

---
+++ logo-art/art-gen.bat
@@ -0,0 +1,18 @@
+@echo off
+REM Create branch (ignore error if it already exists)
+git checkout -b logo-art 2>nul
+IF ERRORLEVEL 1 git checkout logo-art
+
+REM Create a file so we can commit
+echo Legacy commit from Feb 11, 2015> legacy.txt
+git add legacy.txt
+
+REM Set backdated commit time
+set GIT_AUTHOR_DATE=2015-02-11 10:00:00
+set GIT_COMMITTER_DATE=2015-02-11 10:00:00
+
+git commit -m "generate logo art"
+
+echo.
+echo Done. Now publish the branch in GitHub Desktop.
+pause
